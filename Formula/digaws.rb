ac997787930361669ce35112859c07235d1ae267
goreleaserbot <sam@outsidethe.net>
2021-08-03 15:37:54 +0100

    Brew formula update for digaws version v0.3.4

--- Formula/digaws.rb
+++ Formula/digaws.rb
@@ -5,24 +5,24 @@
 class Digaws < Formula
   desc "Look up region and other information for any AWS IP address"
   homepage "https://github.com/sampointer/digaws"
-  version "0.3.3"
+  version "0.3.4"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sampointer/digaws/releases/download/v0.3.3/digaws_0.3.3_Darwin_x86_64.tar.gz"
-      sha256 "3543e645d7080461994276de804853c1b060f189a93b653586bb5714dd3ac56e"
+      url "https://github.com/sampointer/digaws/releases/download/v0.3.4/digaws_0.3.4_Darwin_x86_64.tar.gz"
+      sha256 "f322923f90723adf5b6bcc5e9222898f37743f9c5af59c6855a0453fe8a4800c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sampointer/digaws/releases/download/v0.3.3/digaws_0.3.3_Linux_x86_64.tar.gz"
-      sha256 "178bdf7f524028bacabb67714c4442dd447c16f2edaba6d3a534b458a2494674"
+      url "https://github.com/sampointer/digaws/releases/download/v0.3.4/digaws_0.3.4_Linux_x86_64.tar.gz"
+      sha256 "a1b162a7fbcb51b454d32ca969cc9464f1b5a763c019a2d0d2d285e7c521bc95"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sampointer/digaws/releases/download/v0.3.3/digaws_0.3.3_Linux_arm64.tar.gz"
-      sha256 "fefb1dc9d2bdb7baa2292adfc7eb760405da62a01083db206fd27cf081162aa1"
+      url "https://github.com/sampointer/digaws/releases/download/v0.3.4/digaws_0.3.4_Linux_arm64.tar.gz"
+      sha256 "48c69e993ede30618e9a92c54005473e0e18296b4a370af45991969813e14c2e"
     end
   end
 
